cb7202e4921eb8dc54e6e5a1bbc708bc93a6de10
Zakaria Habri <zakaria.habri@gmail.com>
2025-11-28 15:05:16 +0100

    chore: delete target stuff

--- day-00/target/debug/day-00.d
+++
@@ -1,1 +0,0 @@
-/home/zak/Code/aoc-2025/day-00/target/debug/day-00: /home/zak/Code/aoc-2025/day-00/src/main.rs
--- day-00/target/debug/deps/day_00-86a328ccface6484.d
+++
@@ -1,5 +0,0 @@
-/home/zak/Code/aoc-2025/day-00/target/debug/deps/day_00-86a328ccface6484.d: src/main.rs
-
-/home/zak/Code/aoc-2025/day-00/target/debug/deps/day_00-86a328ccface6484: src/main.rs
-
-src/main.rs:
